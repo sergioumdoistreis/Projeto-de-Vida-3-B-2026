4156578dd423ce2de33a8829e24f647ce1ef48e8
oigresAoContrario <fernandes.santos.sergio@escola.pr.gov.br>
2026-04-15 16:11:35 -0300

    .15/04

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
 
 for (let i = 0; i < botoes.length; i++) {
+    
+    botoes[i].onclick = function(){
+      botoes[i].classList.add("ativo");
 
-    console.log(botoes[i]);
+    }
+    console.log(botoes[i]);    
 
-
-}
+    }
 
 
 
