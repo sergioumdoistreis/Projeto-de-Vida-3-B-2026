2b638ff41de49e4a2fd6592b2075df7f292a8d3f
oigresAoContrario <fernandes.santos.sergio@escola.pr.gov.br>
2026-04-29 16:19:47 -0300

    .

--- main.js
+++ main.js
@@ -15,11 +15,15 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-05-25T23:30:00")
-let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
-
+function calculaTempo(tempoObjetivo){
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = tempoFinal/1000;
+  return segundos;
+}
 
 
 
