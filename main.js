caa0835e490867085d3bbf6431f4576618beb921
oigresAoContrario <fernandes.santos.sergio@escola.pr.gov.br>
2026-04-29 16:09:51 -0300

    .

--- main.js
+++ main.js
@@ -15,8 +15,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-05-25T23:30:00")
+let tempoAtual = new Date();
 
-contadores[0].textContent = "Contagem regressiva";
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
 
 
